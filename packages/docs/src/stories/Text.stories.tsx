365270849235d346b4b3645ac22353d793f3ceef
Guilherme de Oliveira <guizi_oliveira@hotmail.com>
2022-11-20 22:01:12 -0300

    feat: creating component `Text`

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,50 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+
+  args: {
+    size: 'md',
+    children:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro reprehenderit est veniam? Rem recusandae sint perspiciatis debitis corrupti. Deserunt neque exercitationem illo voluptatum laudantium culpa suscipit accusantium blanditiis voluptas numquam.',
+  },
+  argTypes: {
+    size: {
+      options: [
+        'xxs',
+        'xs',
+        'sm',
+        'md',
+        'lg',
+        'xl',
+        '2xl',
+        '4xl',
+        '5xl',
+        '6xl',
+        '7xl',
+        '8xl',
+        '9xl',
+      ],
+      control: {
+        type: 'select',
+      },
+    },
+    as: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+  parameters: {
+    layout: 'centered',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+export const CustonTag: StoryObj<TextProps> = {
+  args: {
+    as: 'strong',
+  },
+}
