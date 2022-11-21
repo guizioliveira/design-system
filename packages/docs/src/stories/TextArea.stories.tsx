8f946712f3afaf6a38e58b97ff1ab7f4267d1899
Guilherme de Oliveira <guizi_oliveira@hotmail.com>
2022-11-21 00:01:36 -0300

    feat: creating `TextArea` component

---
+++ packages/docs/src/stories/TextArea.stories.tsx
@@ -0,0 +1,41 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Box, Text, TextArea, TextAreaProps } from '@visum-ui/react'
+
+export default {
+  title: 'Form/Text Area',
+  component: TextArea,
+  args: {},
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '$2',
+            width: '630px',
+          }}
+        >
+          <Text size="sm">Message</Text>
+          {Story()}
+        </Box>
+      )
+    },
+  ],
+  parameters: {
+    layout: 'centered',
+  },
+} as Meta<TextAreaProps>
+
+export const Primary: StoryObj<TextAreaProps> = {
+  args: {
+    placeholder: 'Add any observations...',
+  },
+}
+
+export const Disabled: StoryObj<TextAreaProps> = {
+  args: {
+    disabled: true,
+  },
+}
