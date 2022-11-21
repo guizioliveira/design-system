870dd5126b78a5acf84fb6de0d06468350bbfcde
Guilherme de Oliveira <guizi_oliveira@hotmail.com>
2022-11-21 00:53:01 -0300

    feat: creating `Multi Step` component

---
+++ packages/docs/src/stories/MultiStep.stories.tsx
@@ -0,0 +1,30 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Box, MultiStep, MultiStepProps } from '@visum-ui/react'
+
+export default {
+  title: 'Form/Multi Step',
+  component: MultiStep,
+  args: { size: 4, currentStep: 1 },
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '$2',
+            width: '630px',
+          }}
+        >
+          {Story()}
+        </Box>
+      )
+    },
+  ],
+  parameters: {
+    layout: 'centered',
+  },
+} as Meta<MultiStepProps>
+
+export const Primary: StoryObj<MultiStepProps> = {}
