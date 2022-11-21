393c0e8472ef10eff9e23ce8f8bbdbe5ae82810a
Guilherme de Oliveira <guizi_oliveira@hotmail.com>
2022-11-20 22:34:55 -0300

    feat: creating component `Heading`

---
+++ packages/docs/src/stories/Heading.stories.tsx
@@ -0,0 +1,32 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Heading, HeadingProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Heading',
+  component: Heading,
+
+  args: {
+    size: 'md',
+    children: 'Heading',
+  },
+  argTypes: {
+    size: {
+      options: ['sm', 'md', 'lg', 'xl', '2xl', '4xl', '5xl', '6xl'],
+      control: {
+        type: 'select',
+      },
+    },
+  },
+  parameters: {
+    layout: 'centered',
+  },
+} as Meta<HeadingProps>
+
+export const Primary: StoryObj<HeadingProps> = {}
+export const CustonTag: StoryObj<HeadingProps> = {
+  args: {
+    as: 'h1',
+    children: 'H1 Heading',
+    size: 'lg',
+  },
+}
