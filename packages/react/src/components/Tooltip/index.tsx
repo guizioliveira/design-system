45de12655e48d5edafd802960473ea1588f96c71
Guilherme de Oliveira <guizi_oliveira@hotmail.com>
2023-04-25 21:34:24 -0300

    refactor: applying prettier

--- packages/react/src/components/Tooltip/index.tsx
+++ packages/react/src/components/Tooltip/index.tsx
@@ -1,23 +1,21 @@
-import { ComponentProps, ReactNode } from 'react'
+import { ComponentProps, ReactNode } from "react"
 import * as TooltipRadix from "@radix-ui/react-tooltip"
-import { TooltipContent, TooltipArrow } from './styles'
+import { TooltipContent, TooltipArrow } from "./styles"
 
 export interface TooltipProps extends ComponentProps<typeof TooltipRadix.Root> {
   children: string | ReactNode
   content: string | ReactNode
 }
 
-export function Tooltip({content, children, ...props} : TooltipProps) {
+export function Tooltip({ content, children, ...props }: TooltipProps) {
   return (
     <TooltipRadix.Provider>
       <TooltipRadix.Root {...props} delayDuration={400}>
-        <TooltipRadix.Trigger asChild>
-          {children}
-        </TooltipRadix.Trigger>
+        <TooltipRadix.Trigger asChild>{children}</TooltipRadix.Trigger>
         <TooltipRadix.Portal>
           <TooltipContent sideOffset={6} arrowPadding={12}>
             {content}
-            <TooltipArrow width={16} height={8}/>
+            <TooltipArrow width={16} height={8} />
           </TooltipContent>
         </TooltipRadix.Portal>
       </TooltipRadix.Root>
@@ -25,4 +23,4 @@ export function Tooltip({content, children, ...props} : TooltipProps) {
   )
 }
 
-Tooltip.displayName = 'Tooltip'
+Tooltip.displayName = "Tooltip"
