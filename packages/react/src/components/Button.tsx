e1b5a3f973ce8fc5a198890f9c599fed3e42ae9e
Guilherme de Oliveira <guizi_oliveira@hotmail.com>
2023-04-26 21:31:09 -0300

    feat: adding boxShadow when Button is on focus

--- packages/react/src/components/Button.tsx
+++ packages/react/src/components/Button.tsx
@@ -1,72 +1,76 @@
-import { ComponentProps, ElementType } from 'react'
-import { styled } from '../styles'
+import { ComponentProps, ElementType } from "react"
+import { styled } from "../styles"
 
-export const Button = styled('button', {
-  all: 'unset',
-  borderRadius: '$sm',
-  fontSize: '$sm',
-  fontWeight: '$medium',
-  fontFamily: '$default',
-  textAlign: 'center',
+export const Button = styled("button", {
+  all: "unset",
+  borderRadius: "$sm",
+  fontSize: "$sm",
+  fontWeight: "$medium",
+  fontFamily: "$default",
+  textAlign: "center",
   minWidth: 120,
-  boxSizing: 'border-box',
-  padding: '0 $4',
+  boxSizing: "border-box",
+  padding: "0 $4",
 
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  gap: '$2',
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  gap: "$2",
 
-  cursor: 'pointer',
+  cursor: "pointer",
 
   svg: {
-    width: '$4',
-    height: '$4',
+    width: "$4",
+    height: "$4",
   },
 
-  '&:disabled': {
-    cursor: 'not-allowed',
+  "&:disabled": {
+    cursor: "not-allowed",
+  },
+
+  "&:focus": {
+    boxShadow: "0 0 0 2px $colors$gray100",
   },
 
   variants: {
     variant: {
       primary: {
-        color: '$white',
-        background: '$primary500',
+        color: "$white",
+        background: "$primary500",
 
-        '&:not(:disabled):hover': {
-          background: '$primary300',
+        "&:not(:disabled):hover": {
+          background: "$primary300",
         },
 
-        '&:disabled': {
-          backgroundColor: '$gray200',
+        "&:disabled": {
+          backgroundColor: "$gray200",
         },
       },
 
       secondary: {
-        color: '$primary300',
-        border: '2px solid $primary500',
+        color: "$primary300",
+        border: "2px solid $primary500",
 
-        '&:not(:disabled):hover': {
-          background: '$primary500',
-          color: '$white',
+        "&:not(:disabled):hover": {
+          background: "$primary500",
+          color: "$white",
         },
 
-        '&:disabled': {
-          color: '$gray200',
-          borderColor: '$gray200',
+        "&:disabled": {
+          color: "$gray200",
+          borderColor: "$gray200",
         },
       },
 
       tertiary: {
-        color: '$gray100',
+        color: "$gray100",
 
-        '&:not(:disabled):hover': {
-          color: '$white',
+        "&:not(:disabled):hover": {
+          color: "$white",
         },
 
-        '&:disabled': {
-          color: '$gray600',
+        "&:disabled": {
+          color: "$gray600",
         },
       },
     },
@@ -83,8 +87,8 @@ export const Button = styled('button', {
   },
 
   defaultVariants: {
-    variant: 'primary',
-    size: 'md',
+    variant: "primary",
+    size: "md",
   },
 })
 
@@ -92,4 +96,4 @@ export interface ButtonProps extends ComponentProps<typeof Button> {
   as?: ElementType
 }
 
-Button.displayName = 'Button'
+Button.displayName = "Button"
